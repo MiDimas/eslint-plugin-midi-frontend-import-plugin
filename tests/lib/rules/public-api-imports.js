36edb534b53625e70eeba1a9762ac6bf7f4a0fa6
MiDimas <103483149+MiDimas@users.noreply.github.com>
2024-05-04 19:09:23 +0800

    Завершено новое правило для импорта из publicAPI

--- tests/lib/rules/public-api-imports.js
+++ tests/lib/rules/public-api-imports.js
@@ -16,16 +16,48 @@ const rule = require("../../../lib/rules/public-api-imports"),
 // Tests
 //------------------------------------------------------------------------------
 
-const ruleTester = new RuleTester();
+const ruleTester = new RuleTester({
+  parserOptions: {ecmaVersion: 6, sourceType: 'module'}
+});
 ruleTester.run("public-api-imports", rule, {
   valid: [
     // give me some code that won't trigger a warning
+    {
+      name: 'correct absolute path',
+      code: "import {Component} from 'entities/Article'",
+      filename: 'C:/User/Desktop/project/src/entities/Article',
+    },
+    {
+      name: 'correct absolute path with alias',
+      code: "import {Component} from '@/entities/Article'",
+      options: [{
+        aliasAbsolutePath: '@'
+      }]
+    },
+    {
+      name: 'relative path',
+      code: "import {Component} from './Article'",
+      options: [{
+        aliasAbsolutePath: '@'
+      }]
+    },
   ],
 
   invalid: [
     {
-      code: "import {Component} from 'entities/article/ui/Components/Components';",
-      errors: [{ message: "Fill me in.", type: "Me too" }],
+      name: "incorrect absolute path",
+      code: "import {Component} from 'entities/Article/ui/Components/Components';",
+      errors: [{ message: "Импорт из внешних модулей должен осуществляться только через public API"}],
+      output: "import {Component} from 'entities/Article';"
+    },
+    {
+      name: "incorrect absolute path with alias",
+      code: "import {Component} from '@/entities/Article/ui/Components/Components';",
+      errors: [{ message: "Импорт из внешних модулей должен осуществляться только через public API"}],
+      options: [{
+        aliasAbsolutePath: '@'
+      }],
+      output: "import {Component} from '@/entities/Article';"
     },
   ],
 });
